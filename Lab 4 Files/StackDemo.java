82b1cad5a3b4175a88b9c213c97ba71a32f02a86
David Alexander <davidpaulalexander@gmail.com>
2012-09-14 00:37:16 -0400

    Added associated lab files
    Signed-off-by: David Alexander <davidpaulalexander@gmail.com>

---
+++ Lab 4 Files/StackDemo.java
@@ -0,0 +1,15 @@
+/**
+ *  @author Lewis and Chase
+ *
+ *  Driver to run a demonstration of a stack with a graphical 
+ *  user interface.
+ */
+
+public class StackDemo
+{
+  public static void main(String[] args)
+  {
+    StackGUI newDemo = new StackGUI();
+    newDemo.display();
+  }
+}
